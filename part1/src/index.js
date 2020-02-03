2cf44e4877871a9cd9b50570f94fcffb2cd5a2f4
duke <dukeluo@outlook.com>
2020-02-03 14:22:02 +0800

    feat: complete exercise 1.1 - 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,30 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const Header = (props) => (<h1>{props.course}</h1>);
-    const Part = (props) => (<p>{props.part} {props.exercise}</p>);
+    const Header = (props) => (<h1>{props.name}</h1>);
+    const Part = (props) => (<p>{props.content.name} {props.content.exercises}</p>);
     const Content = (props) => (
         <>
-            <Part part={props.parts[0]} exercise={props.exercises[0]} />
-            <Part part={props.parts[1]} exercise={props.exercises[1]} />
-            <Part part={props.parts[2]} exercise={props.exercises[2]} />
+            {props.parts.map((part, index) => <Part content={part} key={index} />)}
         </>
     );
     const Total = (props) => (<p>Number of exercises {props.count}</p>);
 
-    const course = 'Half Stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
     return (
         <>
-            <Header course={course} />
-            <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-            <Total count={exercises1 + exercises2 + exercises3} />
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total count={course.parts.reduce((acc, part) => acc + part.exercises, 0)} />
         </>
     )
 };
