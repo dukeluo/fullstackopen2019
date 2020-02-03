ec74db1be05a07babcfd430b50ac0ad0507be886
duke <dukeluo@outlook.com>
2020-02-03 12:14:52 +0800

    feat: complete exercise 1.1 and 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const App = () => {
+    const Header = (props) => (<h1>{props.course}</h1>);
+    const Part = (props) => (<p>{props.part} {props.exercise}</p>);
+    const Content = (props) => (
+        <>
+            <Part part={props.parts[0]} exercise={props.exercises[0]} />
+            <Part part={props.parts[1]} exercise={props.exercises[1]} />
+            <Part part={props.parts[2]} exercise={props.exercises[2]} />
+        </>
+    );
+    const Total = (props) => (<p>Number of exercises {props.count}</p>);
+
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+
+    return (
+        <>
+            <Header course={course} />
+            <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+            <Total count={exercises1 + exercises2 + exercises3} />
+        </>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
